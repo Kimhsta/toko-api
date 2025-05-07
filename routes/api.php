df45ea141a06a05548481c018b32284db7a9abbb
tiyan ganang <tiyanganang@192.168.1.2>
2025-05-07 21:37:46 +0700

    barang api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/barang', [BarangController::class, 'index']);
+Route::post('/barang', [BarangController::class, 'store']);
+Route::get('/barang/{nobarcode}', [BarangController::class, 'show']);
+Route::put('/barang/{nobarcode}', [BarangController::class, 'update']);
+Route::delete('/barang/{nobarcode}', [BarangController::class, 'destroy']);
