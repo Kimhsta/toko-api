ddf38d022f19b70f9c80ae438b9014d179862112
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2025-06-24 01:51:59 +0700

    feat: implement login check and loading state in MyApp widget

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
-// import 'package:frontend_flutter/ui/registrasi_page.dart';
+import 'package:frontend_flutter/helpers/user_info.dart';
+import 'package:frontend_flutter/ui/login_page.dart';
 import 'package:frontend_flutter/ui/produk_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/// Widget utama aplikasi
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  /// Widget default saat menunggu pengecekan login
+  Widget page = const Scaffold(
+    body: Center(child: CircularProgressIndicator()),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    isLogin();
+  }
+
+  /// Mengecek apakah user sudah login (berdasarkan token di SharedPreferences)
+  void isLogin() async {
+    var token = await UserInfo().getToken();
+    setState(() {
+      page = (token != null) ? const ProdukPage() : const LoginPage();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Toko Kita',
       debugShowCheckedModeBanner: false,
-      home: ProdukPage(),
+      home: page,
     );
   }
 }
